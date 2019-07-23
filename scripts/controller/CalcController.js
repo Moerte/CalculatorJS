92b12ef3f3cb251f4d90c92f0cc67c5d7bb05849
Moerte <nunoliveira86@gmail.com>
2019-07-23 12:01:41 +0100

    Added Initialize Method

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -3,7 +3,19 @@ class CalcController{
     constructor(){
 
         this._displayCalc = '0';
-        this._dataAtual;
+        this._currentDate;
+        this.initialize();
+    }
+
+    initialize(){
+
+        let displayCalEl = document.querySelector("#display");
+        let dateEl = document.querySelector("#data");
+        let timeEl = document.querySelector("#hora");
+
+        displayCalEl.innerHTML = "45676";
+        dateEl.innerHTML =
+        timeEl.innerHTML = 
     }
 
     get displayCalc(){
@@ -15,11 +27,11 @@ class CalcController{
     }
 
     get dataAtual(){
-        return this._dataAtual;
+        return this._currentDate;
     }
 
     set dataAtual(valor){
-        this.dataAtual = valor;
+        this._currentDate = valor;
     }
 
     
