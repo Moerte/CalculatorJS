3d56a1a3ba83d8088456c6be859a8798c2a0f78f
Moerte <nunoliveira86@gmail.com>
2019-08-23 16:38:24 +0100

    Still Working...

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -7,6 +7,7 @@ class CalcController {
         this._displayCalEl = document.querySelector("#display");
         this._dateEl = document.querySelector("#data");
         this._timeEl = document.querySelector("#hora");
+        
         this._currentDate;
         this.initialize();
         this.initButtonsEvents();
@@ -48,8 +49,42 @@ class CalcController {
     }
     pushOperation(value) {
         this._operation.push(value);
+
+        if (this._operation.length > 3) {
+
+
+
+            this.calc();
+
+        }
+
     }
+
+    calc() {
+
+        let last = this._operation.pop();
+        let result = eval(this._operation.join(""));
+        this._operation = [result, last];
+        this.setLastNumberToDisplay();
+
+    }
+    setLastNumberToDisplay() {
+
+        let lastNumber;
+
+        for (let i = this._operation.length - 1; i >= 0; i--) {
+
+            if (!this.isOperator(this._operation[i])) {
+
+                lastNumber = this._operation[i];
+                break;
+            }
+        }
+        this.displayCalc = lastNumber;
+    }
+
     addOperation(value) {
+
         if (isNaN(this.getLastOperation())) {
 
             if (this.isOperator(value)) {
@@ -63,6 +98,7 @@ class CalcController {
             } else {
 
                 this.pushOperation(value);
+                this.setLastNumberToDisplay();
 
             }
         } else {
@@ -75,6 +111,7 @@ class CalcController {
 
                 let newValue = this.getLastOperation().toString() + value.toString();
                 this._setLastOperation(parseInt(newValue));
+                this.setLastNumberToDisplay();
 
             }
 
